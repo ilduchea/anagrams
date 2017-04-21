e0d10ce3cb3184e6ceb01fd4a188f1868be7ce09
Ilduchea <Ilduchea>
2017-04-21 09:08:08 -0700

    create spec and logic to check if strings are an anagram

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,5 +1,13 @@
 class String
   define_method(:anagrams) do |string|
-    self.==(string)
+    reciever_string_array = self.split('')
+    argument_string_array = string.split('')
+    booleans_array = []
+
+    each_char do |c|
+      booleans_array.push(string.include?(c))
+    end
+
+    !(booleans_array.include?(false))
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,4 +5,9 @@ describe('String#anagrams') do
   it('will check if two letters are the same') do
     expect("a".anagrams("a")).to(eq(true))
   end
+
+  it('will check if two strings have same letters') do
+    expect("ruby".anagrams("bury")).to(eq(true))
+  end
+
 end
