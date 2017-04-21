6412bdb15fcdba860b6a9a4f81e13a9fb3c1b662
Ilduchea <Ilduchea>
2017-04-21 09:10:33 -0700

    create spec to check if it will return false.

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -10,4 +10,8 @@ describe('String#anagrams') do
     expect("ruby".anagrams("bury")).to(eq(true))
   end
 
+  it('will return false if two strings do not have same letters') do
+    expect("ruby".anagrams("test")).to(eq(false))
+  end
+
 end
